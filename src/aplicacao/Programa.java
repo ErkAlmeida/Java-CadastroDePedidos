14ed511ca4d1d10bf3d880af3f6e1942fdd9d8b7
Erick Almeida <erickprofissional@gmail.com>
2020-03-28 17:58:15 -0300

    Projeto criado

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,25 @@
+/*
+ *
+ * Ler os dados de um pedido com N itens (N fornecido pelo usuário). 
+ * Depois, mostrar um sumário do pedido conforme exemplo (próxima página). Nota: o instante do pedido deve ser
+	o instante do sistema: new Date()
+	
+*/
+
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) 
+	{
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
